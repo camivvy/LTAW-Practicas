05bc36b6f30b7ac2ff9426dd9af01a2f8954ace8
cviloria <cv.viloria.2020@alumnos.urjc.es>
2024-03-19 13:31:24 +0100

    cambiosss

--- P1/exam.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-fs.readFile('fich1.txt','utf8',(err,data) => {
-    console.log("A");
-});
-console.log("b");
-const data1 = fs.readFile('fich2.txt','utf8');
-console.log("c");
-//buenos dias
