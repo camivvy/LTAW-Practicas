d45656b202d6499d4e750eafbaf77dd1aa9d35c0
cviloria <cv.viloria.2020@alumnos.urjc.es>
2024-05-04 22:45:41 +0200

    lo de la practica anterior aqui

---
+++ P4/cliente.js
@@ -0,0 +1,28 @@
+//-- Elementos del interfaz
+const display = document.getElementById("display");
+const msg_entry = document.getElementById("msg_entry");
+const usernameInput = document.getElementById("username");
+
+//-- Crear un websocket. Se establece la conexión con el servidor
+const socket = io();
+
+
+socket.on("message", (msg)=>{
+  display.innerHTML += '<p style="color:blue">' + msg + '</p>';
+});
+
+//-- Al apretar el botón se envía un mensaje al servidor
+msg_entry.onchange = () => {
+  if (msg_entry.value) {
+    const message = msg_entry.value.trim();
+    const username = usernameInput.value.trim();
+    if (username && message) {
+      socket.send(username + ": " + message); // Envía nombre de usuario junto con el mensaje
+    } else {
+      alert("Por favor, introduce tu nombre de usuario y un mensaje válido.");
+    }
+  }
+  
+  //-- Borrar el mensaje actual
+  msg_entry.value = "";
+}
---
+++ P4/servidor.js
@@ -0,0 +1,64 @@
+//-- Cargar las dependencias
+const socketServer = require('socket.io').Server;
+const http = require('http');
+const express = require('express');
+const colors = require('colors');
+const PUERTO = 9090;
+const Fecha = new Date();
+
+//-- Crear una nueva aplciacion web
+const app = express();
+
+//-- Crear un servidor, asosiaco a la App de express
+const server = http.Server(app);
+
+//-- Crear el servidor de websockets, asociado al servidor http
+const io = new socketServer(server);
+
+//-- Definir el punto de entrada principal de mi aplicación web
+app.get('/', (req, res) => {
+    res.redirect("/chat.html");
+});
+
+//-- Esto es necesario para que el servidor le envíe al cliente la
+//-- biblioteca socket.io para el cliente
+app.use('/', express.static(__dirname +'/'));
+
+//-- El directorio publico contiene ficheros estáticos
+app.use(express.static('public'));
+
+//------------------- GESTION SOCKETS IO
+//-- Evento: Nueva conexión recibida
+io.on('connect', (socket) => {
+
+    console.log('** NUEVA CONEXIÓN **'.yellow);
+    
+    //manda mensaje de bienvenida unicamente al usuario que se ha conectado
+    socket.emit("message", "¡Bienvenido al chat, escribe para comenzar!");
+    
+    //-- Evento de desconexión
+    socket.on('disconnect', function(){
+      console.log('** CONEXIÓN TERMINADA **'.yellow);
+    });  
+  
+    //-- Mensaje recibido, dependiendo de que se recibe se hace cada cosa
+    socket.on("message", (data)=> {
+      console.log("Mensaje Recibido!: " + data);
+    if (data.endsWith("/")){
+        socket.send("Esperando a recibir comando, si quiere consultar los comandos disponibles utilice: /help")
+    }else if (data.split("/")[1] == 'help') {
+        socket.send("Comandos Disponibles: /list, /hello, /date");
+    } else if(data.split("/")[1] == 'list') {
+        socket.send("Hay " + io.engine.clientsCount + " clientes conectados");
+    }else if(data.split("/")[1] == 'hello'){
+        socket.send('Hola! Bienvenido al chat!!')
+    }else if (data.split("/")[1] == 'date'){
+        socket.send("Fecha: " + Fecha.toLocaleDateString());
+    }else{
+        io.send(data);
+    }
+    });
+  });
+
+server.listen(PUERTO);
+console.log("Escuchando en puerto: " + PUERTO);
