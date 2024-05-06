9454c3af2996f2d4246ebc4c92b5c0409cb134e4
cviloria <cv.viloria.2020@alumnos.urjc.es>
2024-05-06 21:53:52 +0200

    permisos, y app hecha  pero no va el cliente error

--- P4/servidor.js
+++ P4/servidor.js
@@ -6,6 +6,38 @@ const colors = require('colors');
 const PUERTO = 9090;
 const Fecha = new Date();
 
+//-- Cargar el módulo de electron
+const electron = require('electron');
+
+console.log("Arrancando electron...");
+
+//-- Variable para acceder a la ventana principal
+//-- Se pone aquí para que sea global al módulo principal
+let win = null;
+
+//-- Punto de entrada. En cuanto electron está listo,
+//-- ejecuta esta función
+electron.app.on('ready', () => {
+    console.log("Evento Ready!");
+
+    //-- Crear la ventana principal de nuestra aplicación
+    win = new electron.BrowserWindow({
+        width: 600,  //-- Anchura 
+        height: 400,  //-- Altura
+
+        //-- Permitir que la ventana tenga ACCESO AL SISTEMA
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+    
+});
+
+ //-- Cargar interfaz gráfica en HTML
+ win.loadFile("chat.html");
+
+});
+
 //-- Crear una nueva aplciacion web
 const app = express();
 
